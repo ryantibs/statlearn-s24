144a26cb43b4a7115647f638a70e5f97da37d37b
Ryan Tibshirani <ryantibs@gmail.com>
2024-03-18 10:46:22 -0700

    Updates, df version

--- lectures/ridgeless/figs.R
+++ lectures/ridgeless/figs.R
@@ -3,7 +3,6 @@ set.seed(1)
 n = 80; p = 500
 rho = 0.2
 Sig = rho^abs(outer(1:p, 1:p, "-")) # autoregressive
-# Sig = matrix(rho, p, p); diag(Sig) = 1 # equicorrelated
 
 e = eigen(Sig)
 b0 = e$vec[,1] / sqrt(e$val[1])
@@ -28,16 +27,22 @@ for (i in 1:length(lam.vec)) {
 # Finite-sample bias and variance: ridgeless
 r = min(n, p)
 e.hat = eigen(Sig.hat)
-Sig.hat.pinv = e.hat$vec %*% diag(c(1 / e.hat$val[1:r], rep(0, p-r))) %*% t(e.hat$vec)
+Sig.hat.pinv = e.hat$vec %*% diag(c(1/e.hat$val[1:r], rep(0, p-r))) %*% 
+  t(e.hat$vec)
 N = (Id - Sig.hat.pinv %*% Sig.hat)
 b0.emp = t(b0) %*% N %*% Sig %*% N %*% b0 * (n < p)
 v0.emp = sum(diag(Sig.hat.pinv %*% Sig)) / n
   
-# Asymptotic bias and variance: ridge
+# Semi-asymptotic bias and variance: ridge
 gam = p/n
-s = e$val
 
-# Function for solving the Silverstein equation
+# Which eigenvalues to use for the Silverstein equation?
+# s = e$val # finite-sample 
+N = 1000    
+theta = 2*(1:N-1)*pi/(2*N+1)
+s = (1-rho^2)/(1-2*rho*cos(theta)+rho^2)
+
+# Function for numerically olving the Silverstein equation
 silverstein = function(z, g, s,
                        interval = c(0, 50),
                        tol = .Machine$double.eps^0.25, 
@@ -61,7 +66,7 @@ for (i in 1:length(lam.vec)) {
   v.lim[i] = vp / v^2 - 1
 }
 
-# Asymptotic bias and variance: ridgeless
+# Semi-asymptotic bias and variance: ridgeless
 obj = silverstein(0, gam, s)
 v = obj$v; vp = obj$vp
 Res = solve(v * Sig + Id)
@@ -88,15 +93,18 @@ r0.lim = as.numeric(alpha * b0.lim + v0.lim)
 
 pdf(file = "ridge_ar.pdf", height = 5, width = 7)
 par(mar = c(4.25, 4.25, 2.25, 1.25))
-matplot(lam.vec, cbind(r.emp, r.lim), type = "l", lty = 1:2, col = 1:2, 
+matplot(lam.vec, cbind(r.emp, r.lim), 
+        type = "l", lty = 1:2, col = 1:2, 
         xlim = c(1e-5, 1e2), xaxt = "n", log = "x",
         main = "Autoregressive features, aligned signal",
         xlab = expression(lambda), ylab = "Risk")
 points(1e-5, r0.emp, pch = 20, col = 1)
 points(1e-5, r0.lim, pch = 21, cex = 0.7, col = 2)
 axis(side = 1, at = 10^((-5):2), labels = c(0, 10^((-4):2)))
-legend("topleft", c("Finite-sample", "Asymptotic"), lty = 1:2, col = 1:2)
+legend("topleft", c("Finite-sample", "Semi-asymptotic"), 
+       lty = 1:2, col = 1:2)
 graphics.off()
 
+all(r0.emp < r.emp)
 all(r0.lim < r.lim)
 sum(b0^2) * alpha
